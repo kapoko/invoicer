d171ed16353b2df772541e39a691ec430a1ccf2f
kapoko <17455539+kapoko@users.noreply.github.com>
2022-03-24 23:19:56 +0100

    Simplify client list and show number of invoices per client

--- src/commands/listClients.ts
+++ src/commands/listClients.ts
@@ -1,3 +1,15 @@
 import { clients } from "../lib/config";
+import { getInvoicePaths, readInvoiceData } from "../lib/files";
 
-export default () => console.table(clients);
+export default () => {
+  const invoicePaths = getInvoicePaths();
+  const data = readInvoiceData(invoicePaths);
+
+  console.table(
+    clients.map(({ id, company }) => ({
+      id,
+      company,
+      numInv: data.filter((inv) => inv.to === id).length,
+    }))
+  );
+};
