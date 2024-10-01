2590170ee845908a802ed8ee4f9329fd2852776c
kapoko <17455539+kapoko@users.noreply.github.com>
2024-10-01 12:03:22 +0200

    Updated dependencies

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -9,82 +9,82 @@ import { getInvoices } from "../lib/invoice";
 const appRoot = join(__dirname, "..", "..");
 
 export default async (
-  invoiceIds: string[],
-  options: {
-    force: boolean | undefined;
-  }
-) => {
-  const config = getConfig();
-
-  if (!invoiceIds.length && options.force) {
-    throw new Error("For safety you can't use force flag on all invoices");
-  }
-
-  // Get the invoices, all if them if invoiceIds is empty, filter out which should be excluded
-  const invoices = getInvoices(invoiceIds).filter(
-    (i) =>
-      i.invoiceNumber.toString().slice(0, 2) ==
-      config.invoice.onlyGenerateStartingWith?.toString()
-  );
-
-  if (!invoices.length) {
-    throw new Error("No invoices found.");
-  }
-
-  // Launch puppeteer
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
-    headless: "new",
-  });
-
-  let count = 0;
-
-  try {
-    for await (const invoice of invoices) {
-      const dataBinding = { ...invoice, config };
-      const fileName = `${config.invoice.prefix}${invoice.invoiceNumber}.pdf`;
-      const path = join(getOutputDirectory(), fileName);
-
-      // If generated invoice already exists, don't generate it
-      if (!options.force && existsSync(path)) {
-        continue;
-      }
-
-      const templateHtml = readFileSync(
-        join(appRoot, "templates", config.invoice.template),
-        "utf8"
-      );
-      const template = handlebars.compile(templateHtml);
-      const finalHtml = encodeURIComponent(template(dataBinding));
-      const pdfOptions = {
-        format: "a4" as PaperFormat,
-        printBackground: true,
-        path,
-      };
-
-      const page = await browser.newPage();
-      await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
-        waitUntil: "networkidle0",
-      });
-      await page.pdf(pdfOptions);
-
-      console.log(`✨ ${path} generated!`);
-      count++;
+    invoiceIds: string[],
+    options: {
+        force: boolean | undefined;
     }
-  } catch (e) {
-    // Close browser and rethrow
+) => {
+    const config = getConfig();
+
+    if (!invoiceIds.length && options.force) {
+        throw new Error("For safety you can't use force flag on all invoices");
+    }
+
+    // Get the invoices, all if them if invoiceIds is empty, filter out which should be excluded
+    const invoices = getInvoices(invoiceIds).filter(
+        (i) =>
+            i.invoiceNumber.toString().slice(0, 2) ==
+            config.invoice.onlyGenerateStartingWith?.toString()
+    );
+
+    if (!invoices.length) {
+        throw new Error("No invoices found.");
+    }
+
+    // Launch puppeteer
+    const browser = await puppeteer.launch({
+        args: ["--no-sandbox"],
+        headless: true,
+    });
+
+    let count = 0;
+
+    try {
+        for await (const invoice of invoices) {
+            const dataBinding = { ...invoice, config };
+            const fileName = `${config.invoice.prefix}${invoice.invoiceNumber}.pdf`;
+            const path = join(getOutputDirectory(), fileName);
+
+            // If generated invoice already exists, don't generate it
+            if (!options.force && existsSync(path)) {
+                continue;
+            }
+
+            const templateHtml = readFileSync(
+                join(appRoot, "templates", config.invoice.template),
+                "utf8"
+            );
+            const template = handlebars.compile(templateHtml);
+            const finalHtml = encodeURIComponent(template(dataBinding));
+            const pdfOptions = {
+                format: "a4" as PaperFormat,
+                printBackground: true,
+                path,
+            };
+
+            const page = await browser.newPage();
+            await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
+                waitUntil: "networkidle0",
+            });
+            await page.pdf(pdfOptions);
+
+            console.log(`✨ ${path} generated!`);
+            count++;
+        }
+    } catch (e) {
+        // Close browser and rethrow
+        await browser.close();
+        throw e;
+    }
+
+    // Close puppeteer
     await browser.close();
-    throw e;
-  }
 
-  // Close puppeteer
-  await browser.close();
+    // Result message
+    const existing = invoices.length - count;
+    const existingMessage = existing
+        ? `${existing} invoice(s) already existed.`
+        : "";
 
-  // Result message
-  const existing = invoices.length - count;
-  const existingMessage = existing
-    ? `${existing} invoice(s) already existed.`
-    : "";
-
-  console.log("Done! " + existingMessage);
+    console.log("Done! " + existingMessage);
 };
